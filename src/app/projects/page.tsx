7ca86d14bf1a5e95f53318de8ae5c3fbe5cbcef7
Hilaladiii <abdulhilal242@gmail.com>
2025-02-06 20:06:13 +0700

    feat: add information under construction(project page)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function ProjectPage() {
   return (
     <div className="h-full text-accentD flex items-center justify-center text-body">
-      masih ga sempat ngelanjutin gess wkwk
+      Under construction will be completed as soon as possible
     </div>
   );
 }
