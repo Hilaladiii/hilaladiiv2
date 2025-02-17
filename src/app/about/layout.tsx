d46fa9854e363fcba98b1097a224a39f46e7db33
Hilaladiii <abdulhilal242@gmail.com>
2025-02-17 20:56:30 +0700

    feat : add list code showcases

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,10 +1,14 @@
 import SideBarAbout from "@/components/fragments/Sidebar";
+import ListCodeShowcase from "./_components/ListCodeShowcase";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen relative">
       <SideBarAbout />
-      <div className="ml-72 h-full">{children}</div>
+      <div className="ml-72 h-full flex">
+        {children}
+        <ListCodeShowcase />
+      </div>
     </div>
   );
 }
