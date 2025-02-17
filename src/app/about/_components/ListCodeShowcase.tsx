cdffe77ec4f0f763d007d70d8b8fa0e2d4842bf0
Hilaladiii <abdulhilal242@gmail.com>
2025-02-17 21:25:43 +0700

    fix: error comment inside jsx

--- src/app/about/_components/ListCodeShowcase.tsx
+++ src/app/about/_components/ListCodeShowcase.tsx
@@ -4,7 +4,7 @@ import { CODE_SHOWCASES } from "@/constant/code-showcase";
 export default function ListCodeShowcase() {
   return (
     <div className="p-2 w-1/2 h-full flex flex-col">
-      <h1 className="text-secondary6 mb-5 ">// Code snippet showcase:</h1>
+      <h1 className="text-secondary6 mb-5 ">{"// Code snippet showcase:"}</h1>
       <div className="flex flex-col gap-5 h-[600px] overflow-y-scroll custom-scroll">
         {CODE_SHOWCASES.map((showcase, index) => (
           <CardCodeShowcase
