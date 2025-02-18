1ac910531c558dfa7381244be6dfa8430ad4002f
Hilaladiii <abdulhilal242@gmail.com>
2025-02-18 21:20:27 +0700

    feat : add empty state project

---
+++ src/components/fragments/EmptyStateProject.tsx
@@ -0,0 +1,17 @@
+export default function EmptyStateProject() {
+  return (
+    <div>
+      <div className="text-center font-medium text-3xl text-secondary6 mb-4">
+        X__X
+      </div>
+      <p>
+        <span className="text-center text-xl text-white font-semibold block">
+          No matching projects
+        </span>
+        <span className="text-center block text-base text-secondary6">
+          for these technologies
+        </span>
+      </p>
+    </div>
+  );
+}
