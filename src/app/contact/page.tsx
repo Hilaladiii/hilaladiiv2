39d35a68e381ad28281852bbcbe40ef5a6d4f1c9
Hilaladiii <abdulhilal242@gmail.com>
2025-02-18 21:21:15 +0700

    feat : add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactInfo from "@/components/fragments/ContactInfo";
+
+export default function ContactPage() {
+  return (
+    <div className="w-full h-full flex">
+      <ContactInfo />
+      contact page
+    </div>
+  );
+}
