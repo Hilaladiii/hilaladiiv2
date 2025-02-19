c087a6521c616d5b8744b45e2d36ba36b161a3b5
Hilaladiii <abdulhilal242@gmail.com>
2025-02-19 15:10:14 +0700

    feat: header mobile

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -3,6 +3,9 @@
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
+import { motion } from "motion/react";
 
 const NAVIGATIONS = [
   {
@@ -21,6 +24,7 @@ const NAVIGATIONS = [
 
 export default function Header() {
   const pathname = usePathname();
+  const [show, setShow] = useState(false);
   return (
     <header className="text-label flex justify-between text-secondary6 border-b border-lines ">
       {/* DEKSTOP */}
@@ -57,11 +61,49 @@ export default function Header() {
       </div>
 
       {/* MOBILE */}
-      <div className="w-full flex lg:hidden">
-        <Link href="/" className="border-r border-lines py-1.5 pl-5 pr-40">
+      <div className="w-full flex sm:hidden items-center">
+        <Link href="/" className="border-r border-lines py-1.5 pl-5 pr-48">
           Hilaladiii
         </Link>
+        <button onClick={() => setShow(!show)} className="p-2 mx-auto">
+          {show ? <IoMdClose size={25} /> : <IoIosMenu size={25} />}
+        </button>
       </div>
+      {show && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="absolute top-[53px] w-full h-full bg-white/10 backdrop-blur-xl text-white border-t border-lines shadow-lg z-50"
+        >
+          <nav className="flex flex-col">
+            {NAVIGATIONS.map((navigation, index) => (
+              <Link
+                key={index}
+                href={navigation.href}
+                className={cn(
+                  "px-6 py-3 hover:bg-white/10 transition",
+                  pathname == navigation.href && "bg-white/10"
+                )}
+                onClick={() => setShow(false)}
+              >
+                {navigation.label}
+              </Link>
+            ))}
+            <Link
+              href="/contact"
+              className={cn(
+                "px-6 py-3 hover:bg-white/10 transition",
+                pathname == "/contact" && "bg-white/10"
+              )}
+              onClick={() => setShow(false)}
+            >
+              _contact
+            </Link>
+          </nav>
+        </motion.div>
+      )}
     </header>
   );
 }
